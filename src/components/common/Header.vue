a7a787c31e062e1179f7b9aada31a20ad905855b
lovelearningcc <2832440535@qq.com>
2020-08-20 11:34:32 +0800

    1234

---
+++ src/components/common/Header.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="header">
+        <div class="logo">水机网页版</div>
+        <div class="header-right">
+            <div class="header-user-con">
+                <!-- 用户头像 -->
+                <div class="user-avator">
+                    <img src="../../assets/img/img.jpg" />
+                </div>
+                <!-- 用户名下拉菜单 -->
+                <el-dropdown class="user-name" trigger="click" @command="handleCommand">
+                    <span class="el-dropdown-link">
+                        {{username}}
+                        <i class="el-icon-caret-bottom"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item divided command="loginout">退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import bus from '../common/bus';
+export default {
+    data() {
+        return {
+            collapse: false,
+            fullscreen: false,
+            name: 'linxin',
+            message: 2
+        };
+    },
+    computed: {
+        username() {
+            let username = localStorage.getItem('ms_username');
+            return username ? username : this.name;
+        }
+    },
+    methods: {
+        // 用户名下拉菜单选择事件
+        handleCommand(command) {
+            if (command == 'loginout') {
+                localStorage.removeItem('ms_username');
+                this.$router.push('/login');
+            }
+        }
+    },
+    // mounted() {
+    //     if (document.body.clientWidth < 1500) {
+    //         this.collapseChage();
+    //     }
+    // }
+};
+</script>
+<style scoped>
+.header {
+    position: relative;
+    box-sizing: border-box;
+    width: 100%;
+    height: 70px;
+    font-size: 22px;
+    color: #fff;
+}
+.collapse-btn {
+    float: left;
+    padding: 0 21px;
+    cursor: pointer;
+    line-height: 70px;
+}
+
+/* //gcc_add_2020.8.12 */
+.header .logo {
+    float: left;
+    width: 250px;
+    line-height: 70px;
+    margin-left: 10px;
+}
+.header-right {
+    float: right;
+    padding-right: 50px;
+}
+.header-user-con {
+    display: flex;
+    height: 70px;
+    align-items: center;
+}
+
+
+.user-name {
+    margin-left: 10px;
+}
+
+.user-avator img {
+    display: block;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+}
+.el-dropdown-link {
+    color: #fff;
+    cursor: pointer;
+}
+
+</style>
